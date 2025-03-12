113ca284c23f6a35b610cda45efd7d25f6d52ef0
Faisal Farooq <dfaisal59@gmail.com>
2025-03-12 18:09:04 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
-import router from "./router/root.jsx";
+import router from "./router/Root";
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
