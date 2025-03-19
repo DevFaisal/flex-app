3c9aec4be4bbc055e21ff65b3488909a3a629526
Faisal Farooq <dfaisal59@gmail.com>
2025-03-19 10:16:35 +0530

    source implented

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -2,18 +2,23 @@ import { createBrowserRouter } from "react-router";
 import LandingPage from "../pages/landing/LandingPage";
 import RootLayout from "../layout/RootLayout";
 import Second from "../pages/second/Second";
+import TrafficRedirect from "../layout/TrafficRedirect";
 
 const router = createBrowserRouter([
   {
-    path: "",
+    path: "/",
     element: <RootLayout />,
     children: [
       {
-        path: "",
+        index: true, 
+        element: <TrafficRedirect />,
+      },
+      {
+        path: "landing",
         element: <LandingPage />,
       },
       {
-        path: "/second",
+        path: "second",
         element: <Second />,
       },
     ],
