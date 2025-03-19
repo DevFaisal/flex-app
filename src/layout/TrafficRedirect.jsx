6b3279d1d781653aaeb48c11a5a375c7e042db5e
Faisal Farooq <dfaisal59@gmail.com>
2025-03-19 10:42:48 +0530

    more links added

--- src/layout/TrafficRedirect.jsx
+++ src/layout/TrafficRedirect.jsx
@@ -1,22 +1,28 @@
 import { useEffect } from "react";
 
 function TrafficRedirect() {
-  console.log("injected....");
   useEffect(() => {
     const isSocialTraffic = () => {
       const referrer = document.referrer ? document.referrer.toLowerCase() : "";
       localStorage.setItem("source", referrer);
       if (!referrer) return false;
 
-      const socialDomains = [
-        "twitter.com",
-        "facebook.com",
-        "instagram.com",
-        "linkedin.com",
-        "t.co",
-        "pinterest.com",
-        "reddit.com",
-      ];
+       const socialDomains = [
+         "twitter.com",
+         "t.co",
+         "facebook.com",
+         "instagram.com",
+         "linkedin.com",
+         "pinterest.com",
+         "reddit.com",
+         "tiktok.com",
+         "snapchat.com",
+         "youtube.com",
+         "whatsapp.com",
+         "t.me",
+         "discord.com",
+         "quora.com",
+       ];
 
       return socialDomains.some((domain) => referrer.includes(domain));
     };
