e615fb9d67113e8ddd4984278fba33d47310609b
Faisal Farooq <dfaisal59@gmail.com>
2025-03-26 16:48:47 +0530

    Integrate react-cookie-consent for cookie management; update package dependencies in package.json and package-lock.json.

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Footer from "../components/view/Footer";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 
 const RootLayout = () => {
   return (
     <div>
       <Outlet />
       <Footer />
+      <CookieConsent
+        enableDeclineButton
+        location="bottom"
+        buttonText="Accept"
+        declineButtonText="Decline"
+        cookieName="myConcent"
+        style={{ background: "#2B373B", padding: "20px" }}
+        buttonStyle={{ background: "white", padding: "10px 20px" }}
+        declineButtonStyle={{ background: "gray", padding: "10px 20px" }}
+        expires={150}
+        setDeclineCookie={true}
+        onAccept={() => {
+          Cookies.set("myConcent", "true", { expires: 150 });
+        }}
+        onDecline={() => {
+          Cookies.set("myConcent", "false", { expires: 150 });
+        }}
+      >
+        This website uses will store your personal information like email and name to enhance your experience. By
+        clicking "Accept", you consent to the use of ALL cookies.
+      </CookieConsent>
     </div>
   );
 };
