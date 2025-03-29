f34c415a2127a085a0946c0314063c320aba7ba0
Faisal Farooq <dfaisal59@gmail.com>
2025-03-29 22:37:18 +0530

    aadnewfix

--- vite.config.js
+++ vite.config.js
@@ -4,53 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 import imagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    imagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: "removeViewBox",
-          },
-          {
-            name: "removeEmptyAttrs",
-            active: false,
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          ui: ["primereact"],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
