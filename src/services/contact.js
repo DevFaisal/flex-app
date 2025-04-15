0e71a0311331e95dfd24469d8cef538662b0c191
faisal-flex <faisal@flexcard.app>
2025-04-15 08:52:51 +0530

    Refactor contact service to use API utility for creating contacts; remove commented-out axios implementation for clarity.

--- src/services/contact.js
+++ src/services/contact.js
@@ -4,23 +4,9 @@ import { api } from '../utils/api';
 const baseUrl = 'http://localhost:3000';
 
 const contactService = {
-  // createContact: async (data) => {
-  //   try {
-  //     const response = await api.post('', data);
-
-  //     return response.data;
-  //   } catch (error) {
-  //     console.error('Error Occurred:', error.response?.data || error.message);
-  //     throw error;
-  //   }
-  // },
   createContact: async (data) => {
     try {
-      const response = await axios.post(`${baseUrl}/api/create-contact`, data, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await api.post('', data);
 
       return response.data;
     } catch (error) {
@@ -28,6 +14,20 @@ const contactService = {
       throw error;
     }
   },
+  // createContact: async (data) => {
+  //   try {
+  //     const response = await axios.post(`${baseUrl}/api/create-contact`, data, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     });
+
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error('Error Occurred:', error.response?.data || error.message);
+  //     throw error;
+  //   }
+  // },
 };
 
 export default contactService;
